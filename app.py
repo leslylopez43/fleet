1dcca66b06c522cf244b8e11e24f2e3660eab593
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2024-06-04 11:37:54 +0100

    add html

--- app.py
+++ app.py
@@ -413,10 +413,12 @@ def update_lessor(id):
 
 
 # Function to get database connection
+
 def get_db_connection():
-    conn = sqlite3.connect('your_database.db')
-    conn.row_factory = sqlite3.Row
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row  # To get dict-like rows
     return conn
+    
 
 @app.route('/search', methods=['GET'])
 def search_form():
